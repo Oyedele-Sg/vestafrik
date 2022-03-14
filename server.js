b9b673bfdc935c5b74f203bd306dba8653df14e1
Segun Oyedele <oyedelesegunfunmi@gmail.com>
2022-03-13 20:41:41 -0500

    Prepared for deployment

--- server.js
+++ server.js
@@ -11,14 +11,6 @@ connectDB();
 //Init Middleware
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 //Define Routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/users', require('./routes/api/users'));
@@ -28,6 +20,14 @@ app.use('/api/sales', require('./routes/api/sales'));
 app.use('/api/contact', require('./routes/api/contact'));
 app.use('/api/subscribe', require('./routes/api/subscribe'));
 
-const PORT = process.env.PORT || 5005;
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
